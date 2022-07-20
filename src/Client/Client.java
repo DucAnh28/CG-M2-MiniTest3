1ae3ef884ff6fc48e909feab9336fb26ccb3f1dc
DucAnh28 <ducanhnguyeny2k@gmail.com>
2022-07-20 17:23:58 +0700

    making add meat

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,9 +1,29 @@
 package Client;
 
+import Controller.MaterialManager;
+import Model.Material;
+import Model.Meat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Client {
+    public static List<Material> list = MaterialManager.list;
     public static void main(String[] args) {
 
     }
 
+    public static Material addMeat(){
+        Meat meat = new Meat();
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.print("Enter ID: ");
+        meat.setId(scanner.nextLine());
+        System.out.print("Enter Name: ");
+        meat.setName(scanner1.nextLine());
+        System.out.print("Enter Manufactureing Date: ");
+        meat.setManufacturingDate(scanner1.ne);
 
+    }
 }
